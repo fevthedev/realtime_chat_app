27cf99d51202486a1ca0ba264c07eca160ea2d38
Christopher Fevrier <mrfevrier@users.noreply.github.com>
2023-12-20 13:27:43 -0400

    TODO updates

--- server.js
+++ server.js
@@ -19,3 +19,10 @@ io.on('connection', (socket) => {
 http.listen(3000, () => {
 	console.log('Chat server running at http://localhost:3000');
 });
+
+// TODO:
+/*
+1. Customize HTML interface
+2. User authentication
+3. Implement contact list
+ */
