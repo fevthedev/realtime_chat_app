28b343c45caef6a63d5afc2f626aa86da09a662b
Christopher Fevrier <mrfevrier@users.noreply.github.com>
2023-12-20 13:23:44 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+	console.log('User connected');
+	socket.on('disconnect', () => {
+		console.log('User disconnected.');
+	});
+
+	socket.on('chat message', (msg) => {
+		io.emit('chat message', msg);
+	});
+});
+
+http.listen(3000, () => {
+	console.log('Chat server running at http://localhost:3000');
+});
